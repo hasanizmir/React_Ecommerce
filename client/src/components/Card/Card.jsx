bbded578edd686a3a09af897fd335d61d092334c
Hasan İzmir <hasanizmirr@gmail.com>
2023-01-25 14:18:04 +0300

    feat:FeaturedProduct api

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './Card.scss';
+import "./Card.scss";
 
-const Card = ({item}) => {
+const Card = ({ item }) => {
   return (
-    <Link className='link' to={`/product/${item.id}`}>
-      <div className='card'>
+    <Link className="link" to={`/product/${item.id}`}>
+      <div className="card">
         <div className="image">
-          {item.isNew && <span>New Season</span>}
-          <img src={item.img} alt={item.name} className="mainImg" />
-          <img src={item.img2} alt={item.name} className="secondImg" />
+          {item?.attributes?.isNew && <span>New Season</span>}
+          <img
+            src={
+              process.env.REACT_APP_UPLOAD_URL +
+              item?.attributes?.image?.data?.attributes?.url
+            }
+            alt={item.name}
+            className="mainImg"
+          />
+          <img
+            src={
+              process.env.REACT_APP_UPLOAD_URL +
+              item?.attributes?.img2?.data?.attributes?.url
+            }
+            alt={item.name}
+            className="secondImg"
+          />
         </div>
-        <h2>{item.title}</h2>
+        <h2>{item?.attributes?.title}</h2>
         <div className="prices">
-          <h3>$ {item.oldPrice}</h3>
-          <h3>$ {item.price}</h3>
+          <h3>$ {item.oldPrice || item?.attributes?.price + 20}</h3>
+          <h3>$ {item?.attributes?.price}</h3>
         </div>
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
