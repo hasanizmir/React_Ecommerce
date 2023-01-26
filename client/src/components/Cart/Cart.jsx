4ddd3a5b985c7d108fc3c41d4fafac063481f5de
Hasan İzmir <hasanizmirr@gmail.com>
2023-01-26 12:55:01 +0300

    feat:Stripe configuration

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 import { useDispatch, useSelector } from "react-redux";
+import { loadStripe } from "@stripe/stripe-js";
 
 import "./Cart.scss";
 import { removeItem, resetCart } from "../../redux/cartReducer";
+import makeRequest from "../../makeRequest";
 
 const Cart = () => {
   const products = useSelector((state) => state.cart.products);
@@ -15,6 +17,25 @@ const Cart = () => {
       total += item.quantity * item.price;
     });
     return total.toFixed(2);
+  };
+
+  const stripePromise = loadStripe(
+    "pk_test_eOTMlr8usx1ctymXqrik0ls700lQCsX2UB"
+  );
+
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+
+      const res = await makeRequest.post("/orders", {
+        products,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -42,7 +63,7 @@ const Cart = () => {
             <span>SUBTOTAL</span>
             <span>${totalPrice()}</span>
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
           <span className="reset" onClick={() => dispatch(resetCart())}>
             Delete all item
           </span>
